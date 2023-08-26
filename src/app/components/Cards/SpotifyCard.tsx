72388e139f7234ae270ea9eab0936f9cb48c0420
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-08-26 13:43:50 -0400

    Update SpotifyCard.tsx

--- src/app/components/Cards/SpotifyCard.tsx
+++ src/app/components/Cards/SpotifyCard.tsx
@@ -1,32 +1,72 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Card, CardBody, Divider, Image, Link, Progress } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
 export default function SpotifyCard({ props }: any) {
+
+    const [songProgress, setSongProgress] = useState(0);
+
+    useEffect(() => {
+
+        let interval: any;
+
+        if (props?.data !== undefined) {
+            setSongProgress(props.data.progress);
+            interval = setInterval(() => {
+                setSongProgress((x) => x + 1000)
+            }, 1000)
+
+        }
+
+        return () => clearInterval(interval);
+    }, [props])
+
     return (
-        <Link target="_blank" href={props?.data.songUrl} >
-            <Card isPressable >
-                <CardBody className="p-2">
-                    <div className="flex items-center">
-                        <Image
-                            alt="Album cover"
-                            className="object-cover"
-                            shadow="md"
-                            src={props?.data.albumImageUrl}
-                            width="50px"
-                        />
-                        <div className="ml-2">
-                            <div className="flex items-center ">
-                                <Icon width={15} icon={"mdi:spotify"} /><p className="text-sm ml-1">Listening to Spotify</p>
+        <div>
+
+            {props?.data ?
+                <Link target="_blank" href={props?.data.songUrl} >
+                    <Card isPressable >
+                        <CardBody className="p-2">
+                            <div className="flex items-center">
+                                <Image
+                                    alt="Album cover"
+                                    className="object-cover"
+                                    shadow="md"
+                                    src={props?.data.albumImageUrl}
+                                    width="50px"
+                                />
+                                <div className="ml-2">
+                                    <div className="flex items-center ">
+                                        <Icon width={15} icon={"mdi:spotify"} /><p className="text-sm ml-1">Listening to Spotify</p>
+                                    </div>
+
+                                    <p className="text-sm ml-1">{props?.data.title} - {props?.data.artist[0].name}</p>
+                                    <Progress color="success" className="mt-1" size="sm" aria-label="Loading..." value={songProgress} maxValue={props?.data.duration} />
+                                </div>
+
                             </div>
+                        </CardBody>
+                    </Card>
+                </Link>
+                :
 
-                            <p className="text-sm ml-1">{props?.data.title} - {props?.data.artist[0].name}</p>
+                <Card isPressable >
+                    <CardBody className="p-2">
+                        <div className="flex items-center">
+                            <Icon icon="mdi:spotify" width={50} />
+                            <div className="ml-2">
+                                <div className="flex items-center ">
+                                    <Icon width={15} icon={"mdi:mute"} /><p className="text-sm ml-1">Not listening to anything</p>
+                                </div>
+                                <p className="text-sm ml-1">Concentrating...</p>
+
+                            </div>
                         </div>
+                    </CardBody>
+                </Card>
 
-
-
-                    </div>
-                </CardBody>
-            </Card>
-        </Link>
+            }
+        </div>
     )
 }
