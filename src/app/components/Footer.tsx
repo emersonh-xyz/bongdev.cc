a4b1988da98f49e58f51c9e76cf78c49027c4af2
Emerson <70560728+emersonh-xyz@users.noreply.github.com>
2023-09-08 16:56:04 -0400

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="mt-8 flex flex-row  bottom-0 ">
+        <div className="mt-8 flex flex-row bottom-0 mb-8">
             <p className="font-light">@ Emerson Hicks 2023</p>
             <p></p>
         </div>
